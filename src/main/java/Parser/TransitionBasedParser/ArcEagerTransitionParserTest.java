6922353f1806e2bb0213167447482989f52390ca
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-05-03 15:17:13 +0300

    Added C45 models and model saving example test cases for ArcEager and ArcStandard parsers.

--- src/main/java/Parser/TransitionBasedParser/ArcEagerTransitionParserTest.java
+++ src/main/java/Parser/TransitionBasedParser/ArcEagerTransitionParserTest.java
@@ -1,6 +1,9 @@
 package Parser.TransitionBasedParser;
 
+import Classification.Classifier.C45;
+import Classification.DataSet.DataSet;
 import Classification.Model.Model;
+import Classification.Parameter.C45Parameter;
 import DependencyParser.ParserEvaluationScore;
 import DependencyParser.Universal.UniversalDependencyTreeBankCorpus;
 import DependencyParser.Universal.UniversalDependencyTreeBankSentence;
@@ -13,16 +16,35 @@ import java.io.ObjectInputStream;
 
 public class ArcEagerTransitionParserTest {
 
-    private static Model loadModel(String fileName) {
-        ObjectInputStream outObject;
-        Model model = null;
-        try {
-            outObject = new ObjectInputStream(FileUtils.getInputStream(fileName));
-            model = (Model) outObject.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
+    private void generateEagerModelC45(String language, String dataSetName, int windowSize){
+        UniversalDependencyTreeBankCorpus corpus = new UniversalDependencyTreeBankCorpus(language + "_" + dataSetName + "-ud-train.conllu");
+        TransitionParser transitionParser = new ArcEagerTransitionParser();
+        DataSet dataSet = transitionParser.simulateParse(corpus, windowSize);
+        C45 c45 = new C45();
+        c45.train(dataSet.getInstanceList(), new C45Parameter(1, true, 0.2));
+        c45.getModel().saveTxt(language + "_" + dataSetName + "_eager_c45_" + windowSize + ".txt");
+    }
+
+    @Test
+    public void test(){
+        generateEagerModelC45("tr", "framenet", 3);
+    }
+
+    @Test
+    public void testOracle() {
+        ParserEvaluationScore scores = new ParserEvaluationScore();
+        TransitionParser transitionParser = new ArcEagerTransitionParser();
+        UniversalDependencyTreeBankCorpus corpus = new UniversalDependencyTreeBankCorpus("tr_boun-ud-test.conllu");
+        C45 c45 = new C45();
+        c45.loadModel("models/tr_boun_eager_c45.txt");
+        for (int i = 0; i < corpus.sentenceCount(); i++) {
+            UniversalDependencyTreeBankSentence actual = (UniversalDependencyTreeBankSentence) corpus.getSentence(i);
+            UniversalDependencyTreeBankSentence expected = transitionParser.dependencyParse(actual, new ArcStandardOracle(c45.getModel(), 2));
+            scores.add(actual.compareParses(expected));
         }
-        return model;
+        Assert.assertEquals(64.89458453906572, 100 * scores.getLS(), 0.01);
+        Assert.assertEquals(47.110376188507644, 100 * scores.getLAS(), 0.01);
+        Assert.assertEquals(54.91525423728817, 100 * scores.getUAS(), 0.01);
     }
 
 }
--- src/main/java/Parser/TransitionBasedParser/ArcStandardTransitionParserTest.java
+++ src/main/java/Parser/TransitionBasedParser/ArcStandardTransitionParserTest.java
@@ -1,39 +1,41 @@
 package Parser.TransitionBasedParser;
 
+import Classification.Classifier.C45;
+import Classification.DataSet.DataSet;
 import Classification.Model.Model;
+import Classification.Parameter.C45Parameter;
 import DependencyParser.ParserEvaluationScore;
 import DependencyParser.Universal.UniversalDependencyTreeBankCorpus;
 import DependencyParser.Universal.UniversalDependencyTreeBankSentence;
-import Util.FileUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-
 public class ArcStandardTransitionParserTest {
 
-    private static Model loadModel(String fileName) {
-        ObjectInputStream outObject;
-        Model model = null;
-        try {
-            outObject = new ObjectInputStream(FileUtils.getInputStream(fileName));
-            model = (Model) outObject.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return model;
+    private void generateStandardModelC45(String language, String dataSetName, int windowSize){
+        UniversalDependencyTreeBankCorpus corpus = new UniversalDependencyTreeBankCorpus(language + "_" + dataSetName + "-ud-train.conllu");
+        TransitionParser transitionParser = new ArcStandardTransitionParser();
+        DataSet dataSet = transitionParser.simulateParse(corpus, windowSize);
+        C45 c45 = new C45();
+        c45.train(dataSet.getInstanceList(), new C45Parameter(1, true, 0.2));
+        c45.getModel().saveTxt(language + "_" + dataSetName + "_standard_c45" + windowSize + ".txt");
     }
 
     @Test
-    public void testC1Oracle() {
+    public void test(){
+        generateStandardModelC45("tr", "framenet", 3);
+    }
+
+    @Test
+    public void testOracle() {
         ParserEvaluationScore scores = new ParserEvaluationScore();
         TransitionParser transitionParser = new ArcStandardTransitionParser();
         UniversalDependencyTreeBankCorpus corpus = new UniversalDependencyTreeBankCorpus("tr_boun-ud-test.conllu");
-        Model model = loadModel("decision-tree-boun-1");
+        C45 c45 = new C45();
+        c45.loadModel("models/tr_boun_standard_c45.txt");
         for (int i = 0; i < corpus.sentenceCount(); i++) {
             UniversalDependencyTreeBankSentence actual = (UniversalDependencyTreeBankSentence) corpus.getSentence(i);
-            UniversalDependencyTreeBankSentence expected = transitionParser.dependencyParse(actual, new ArcStandardOracle(model, 2));
+            UniversalDependencyTreeBankSentence expected = transitionParser.dependencyParse(actual, new ArcStandardOracle(c45.getModel(), 2));
             scores.add(actual.compareParses(expected));
         }
         Assert.assertEquals(64.89458453906572, 100 * scores.getLS(), 0.01);
