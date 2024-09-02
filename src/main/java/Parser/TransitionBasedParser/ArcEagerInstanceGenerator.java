6f386594056d95946c9a16529f71e3deb19d1c76
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2024-09-02 18:49:19 +0300

    Added comment lines for methods.

--- src/main/java/Parser/TransitionBasedParser/ArcEagerInstanceGenerator.java
+++ src/main/java/Parser/TransitionBasedParser/ArcEagerInstanceGenerator.java
@@ -10,6 +10,12 @@ import java.util.ArrayList;
 
 public class ArcEagerInstanceGenerator extends InstanceGenerator {
 
+    /**
+     * Checks if the given word has a valid relation.
+     * @param word The UniversalDependencyTreeBankWord to check.
+     * @return true if the relation is valid, false otherwise.
+     */
+
     private boolean suitable(UniversalDependencyTreeBankWord word) {
         try {
             word.getRelation().toString();
@@ -18,6 +24,15 @@ public class ArcEagerInstanceGenerator extends InstanceGenerator {
         }
         return true;
     }
+
+    /**
+     * Generates an Instance object based on the provided state, window size, and command.
+     * The Instance is populated with attributes derived from the words in the state.
+     * @param state The state used to generate the instance.
+     * @param windowSize The size of the window used to extract words from the state.
+     * @param command The command associated with the instance.
+     * @return The generated Instance object.
+     */
 
     @Override
     public Instance generate(State state, int windowSize, String command) {
