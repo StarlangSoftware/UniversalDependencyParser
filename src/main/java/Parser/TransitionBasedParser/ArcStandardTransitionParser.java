20d80ea09cf218bf803723ef8ed286bf0216ea0c
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2024-09-05 12:20:23 +0300

    Added comment lines for methods.

--- src/main/java/Parser/TransitionBasedParser/ArcStandardTransitionParser.java
+++ src/main/java/Parser/TransitionBasedParser/ArcStandardTransitionParser.java
@@ -14,6 +14,13 @@ public class ArcStandardTransitionParser extends TransitionParser {
         super();
     }
 
+    /**
+     * Checks if there are more relations with a specified ID in the list of words.
+     * @param wordList The list of words to check.
+     * @param id The ID to check for.
+     * @return True if no more relations with the specified ID are found; false otherwise.
+     */
+
     private boolean checkForMoreRelation(ArrayList<StackWord> wordList, int id) {
         for (StackWord word : wordList) {
             if (word.getWord().getRelation().to() == id) {
@@ -22,6 +29,13 @@ public class ArcStandardTransitionParser extends TransitionParser {
         }
         return true;
     }
+
+    /**
+     * Simulates the parsing process for a given sentence using the Arc Standard parsing algorithm.
+     * @param sentence The sentence to be parsed.
+     * @param windowSize The size of the window used for feature generation.
+     * @return An ArrayList of {@link Instance} objects representing the parsed actions.
+     */
 
     public ArrayList<Instance> simulateParse(UniversalDependencyTreeBankSentence sentence, int windowSize) {
         UniversalDependencyTreeBankWord top, beforeTop;
@@ -74,6 +88,13 @@ public class ArcStandardTransitionParser extends TransitionParser {
         }
         return instanceList;
     }
+
+    /**
+     * Performs dependency parsing on the given sentence using the provided oracle.
+     * @param universalDependencyTreeBankSentence The sentence to be parsed.
+     * @param oracle The oracle used to make parsing decisions.
+     * @return The parsed sentence with dependency relations established.
+     */
 
     public UniversalDependencyTreeBankSentence dependencyParse(UniversalDependencyTreeBankSentence universalDependencyTreeBankSentence, Oracle oracle) {
         UniversalDependencyTreeBankSentence sentence = createResultSentence(universalDependencyTreeBankSentence);
